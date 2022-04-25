8d5b8365fda40f8fa68320af3f38cd34c39b8612
Stipo Margić <stipo@liberato.io>
2022-04-25 10:23:32 +0200

    User Entity tweak

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,59 +3,51 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiFilter;
-use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Serializer\Annotation\Groups;
 
 
-#[ApiResource(
-    denormalizationContext: ['groups' => ['write']],
-    normalizationContext: ['groups' => ['read']],
-)]
+#[ApiResource]
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    #[Groups(["read"])]
-    #[ORM\Id]
-    #[ORM\Column(type: 'string', unique: true)]
-    #[ORM\GeneratedValue(strategy: "CUSTOM")]
-    #[ORM\CustomIdGenerator(class: "doctrine.uuid_generator")]
+    public const ROLE_ADMIN = "ROLE_ADMIN";
+    public const ROLE_USER = "ROLE_USER";
+    #[
+        ORM\Id,
+        ORM\Column(type: 'string', unique: true),
+        ORM\GeneratedValue(strategy: "CUSTOM"),
+        ORM\CustomIdGenerator(class: "doctrine.uuid_generator")
+    ]
     private string $id;
 
-    #[Groups(["read", "write"])]
     #[ORM\Column(type: 'string', length: 180, unique: true)]
     private string $email;
 
-    #[Groups(["read", "write"])]
     #[ORM\Column(type: 'json')]
-    private array $roles = [];
+    private array $roles;
 
-    #[Groups("write")]
     #[ORM\Column(type: 'string')]
     private string $password;
 
 
-    #[Groups(["read", "write", "post"])]
     #[ApiFilter(SearchFilter::class, strategy: 'ipartial')]
     #[ORM\Column(type: 'string', length: 255)]
     private string $username;
 
-    #[Groups(["read"])]
     #[ORM\Column(type: 'datetime_immutable')]
     private \DateTimeImmutable $createdAt;
 
-    #[Groups(["read"])]
     #[ORM\Column(type: 'datetime_immutable', nullable: true)]
     private ?\DateTimeImmutable $updatedAt;
 
-    #[Groups(["read"])]
     #[ORM\Column(type: 'datetime_immutable', nullable: true)]
     private ?\DateTimeImmutable $deletedAt;
 
@@ -67,6 +59,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->createdAt = new \DateTimeImmutable("now");
         $this->updatedAt = null;
         $this->deletedAt = null;
+        $this->roles[] = $this::ROLE_USER;
         $this->posts = new ArrayCollection();
     }
 
@@ -89,7 +82,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function getUserIdentifier(): string
     {
-        return (string) $this->email;
+        return (string)$this->email;
     }
 
     /**
@@ -97,16 +90,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getRoles(): array
     {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
-
-        return array_unique($roles);
+        return $this->roles;
     }
 
     public function setRoles(string $role): self
     {
-        $this->roles = [$role];
+        $this->roles[] = $role;
 
         return $this;
     }
