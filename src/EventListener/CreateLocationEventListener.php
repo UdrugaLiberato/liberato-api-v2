245994b9f9c597958f3ac23391501eb6001c2b67
Stipo Margić <stipo@liberato.io>
2022-05-14 13:58:23 +0200

    CreateLocationEventListener

---
+++ src/EventListener/CreateLocationEventListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\Location;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class CreateLocationEventListener
+{
+    public function __construct(private TokenStorageInterface $token)
+    {
+    }
+
+    public function prePersist(LifecycleEventArgs $args): void
+    {
+        $entity = $args->getObject();
+
+        if (!$entity instanceof Location) {
+            return;
+        }
+
+        $user = $this->token?->getToken()?->getUser();
+        if (!$user) {
+            $entity->setUser(null);
+        } else {
+            $entity->setUser($user);
+        }
+    }
+}
