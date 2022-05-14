f6b88943476520ca607db0369f32b5a7f0287219
Stipo Margić <stipo@liberato.io>
2022-05-14 13:14:38 +0200

    Location DataTransformer

--- src/DTO/Location/LocationOutput.php
+++ src/DTO/Location/LocationOutput.php
@@ -2,7 +2,27 @@
 
 namespace App\DTO\Location;
 
+use App\Entity\Category;
+use App\Entity\City;
+use App\Entity\User;
+
 class LocationOutput
 {
-
+    public function __construct(
+        public string   $name,
+        public string   $street,
+        public string   $phone,
+        public string   $email,
+        public bool     $published,
+        public bool     $featured,
+        public Category $category,
+        public City     $city,
+        public string   $createdAt,
+        public ?User    $user,
+        public ?string   $about,
+        public ?string  $updatedAt,
+        public ?string  $deletedAt
+    )
+    {
+    }
 }
---
+++ src/DataTransformer/Location/LocationOutputDataTransformer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataTransformer\Location;
+
+use ApiPlatform\Core\DataTransformer\DataTransformerInterface;
+use App\DTO\Location\LocationOutput;
+use App\Entity\Location;
+
+class LocationOutputDataTransformer implements DataTransformerInterface
+{
+
+    public function transform($object, string $to, array $context = []): object
+    {
+        return new LocationOutput(
+            $object->getName(),
+            $object->getStreet(),
+            $object->getPhone(),
+            $object->getEmail(),
+            $object->isPublished(),
+            $object->isFeatured(),
+            $object->getCategory(),
+            $object->getCity(),
+            $object->getCreatedAt()->format('Y-m-d H:i:s'),
+            $object->getUser(),
+            $object->getAbout(),
+            $object->getUpdatedAt()->format('Y-m-d H:i:s'),
+            $object->getDeletedAt()->format('Y-m-d H:i:s')
+        );
+    }
+
+    public function supportsTransformation($data, string $to, array $context = []): bool
+    {
+        return LocationOutput::class === $to && $data instanceof Location;
+
+    }
+}
