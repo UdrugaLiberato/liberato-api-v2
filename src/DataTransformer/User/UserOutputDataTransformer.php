30cdf5a27b359c33b9a81f6d0860775b24a98484
Stipo Margić <stipo@liberato.io>
2022-07-18 18:06:40 +0200

    Added slug

--- src/DataTransformer/User/UserOutputDataTransformer.php
+++ src/DataTransformer/User/UserOutputDataTransformer.php
@@ -6,7 +6,6 @@ use ApiPlatform\Core\DataTransformer\DataTransformerInterface;
 use App\DTO\User\UserOutput;
 use App\Entity\User;
 use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 
 class UserOutputDataTransformer implements DataTransformerInterface
 {
@@ -14,38 +13,48 @@ class UserOutputDataTransformer implements DataTransformerInterface
     {
         $posts = $this->getPostsFromUser($object->getPosts());
         return new UserOutput(
-          $object->getId(),
-          $object->getName(),
-          $object->getEmail(),
-          $object->getRoles()[0],
-          null === $object->getPhone() ? null : $object->getPhone(),
-          null === $object->getFilePath() ? null : $object->getFilePath(),
+            $object->getId(),
+            $object->getName(),
+            $object->getEmail(),
+            $object->getRoles()[0],
+            null === $object->getPhone() ? null : $object->getPhone(),
+            null === $object->getFilePath() ? null : $object->getFilePath(),
             $posts,
-          $object->getCreatedAt()->format('Y-m-d H:i:s'),
-          $object->getUpdatedAt()?->format("Y-m-d H:i:s"),
-          $object->getDeletedAt()?->format("Y-m-d H:i:s")
+            $object->getCreatedAt()->format('Y-m-d H:i:s'),
+            $object->getUpdatedAt()?->format("Y-m-d H:i:s"),
+            $object->getDeletedAt()?->format("Y-m-d H:i:s")
         );
-    }
-    
-    public function supportsTransformation($data, string $to, array $context = []): bool
-    {
-        return UserOutput::class === $to && $data instanceof User;
     }
 
     private function getPostsFromUser($posts): array|ArrayCollection
     {
         if (count($posts->getValues()) === 0) return [];
 
-        $arr = $posts->map(function ($post) {
-                return [
-                    "id" => $post->getId(),
-                    "title" => $post->getTitle(),
-                    "body" => $post->getBody(),
-                    "tags" => $post->getTags(),
-                "image" => "images/posts/". $post->getImages()[0],
-                    "created_at" => $post->getCreatedAt()->format('Y-m-d H:i:s')
-                ];
+        return $posts->map(function ($post) {
+            return [
+                "id" => $post->getId(),
+                "title" => $post->getTitle(),
+                "slug" => $this->slugify($post->getTitle()),
+                "body" => $post->getBody(),
+                "tags" => $post->getTags(),
+                "image" => "images/posts/" . $post->getImages()[0],
+                "created_at" => $post->getCreatedAt()->format('Y-m-d H:i:s')
+            ];
         });
-        return $arr;
+    }
+
+    private function slugify(string $title): string
+    {
+        $title = preg_replace('~[^\pL\d]+~u', '-', $title);
+        $title = iconv('utf-8', 'us-ascii//TRANSLIT', $title);
+        $title = preg_replace('~[^-\w]+~', '', $title);
+        $title = trim($title, '-');
+        $title = preg_replace('~-+~', '-', $title);
+        return strtolower($title);
+    }
+
+    public function supportsTransformation($data, string $to, array $context = []): bool
+    {
+        return UserOutput::class === $to && $data instanceof User;
     }
 }
