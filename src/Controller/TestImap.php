274ebe211976b74b8a357cbcec08244b8a64f830
Stipo Margić <stipo@udruga-liberato.hr>
2023-07-19 20:00:24 +0200

    IMAP Funcionality

---
+++ src/Controller/TestImap.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Emails;
+use App\Repository\EmailsRepository;
+use SecIT\ImapBundle\Service\Imap;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestImap extends AbstractController {
+  public const BACKEND_URL_FILES = 'https://dev.udruga-liberato.hr/uploads/emails_attachments/';
+
+  private string $uploadDir;
+
+  public function __construct(private readonly KernelInterface  $kernel,
+                              private readonly EmailsRepository $emailsRepository) {
+    $this->uploadDir = $this->kernel->getProjectDir() . '/public/uploads/email_attachments/';
+  }
+
+  #[Route('/check', name: 'check')]
+  public function index(Imap $imap) {
+    $connection = $imap->get('liberato_imap');
+
+    $mail_ids = $connection->searchMailbox('UNSEEN');
+    foreach ($mail_ids as $i) {
+      if ($this->emailsRepository->findOneBy(['messageId' => $i])) {
+        break;
+      }
+      $mail = $connection->getMail($i, true);
+      $attachments = $mail->getAttachments();
+      $emailEntity = new Emails();
+      $emailEntity->setMessageId($i);
+      $emailEntity->setSubject($mail->subject);
+      $emailEntity->setFromAddress($mail->fromAddress);
+      $emailEntity->setFromName($mail->fromName);
+      $emailEntity->setBody($mail->textHtml);
+      $emailEntity->setDate($mail->date);
+      $attachmentsArray = [];
+      foreach ($attachments as $attachment) {
+        echo '--> Saving ' . (string)$attachment->name . '...';
+
+        $filename = time() . '_' . $attachment->name;;
+        $filePath = $this->uploadDir .  $filename;
+        $attachment->setFilePath($filePath);
+
+        if ($attachment->saveToDisk()) {
+          echo "OK, saved!\n";
+        } else {
+          echo "ERROR, could not save!\n";
+        }
+
+        $attachmentsArray[] = [
+            "name" => $attachment->name,
+            "filePath" => self::BACKEND_URL_FILES . $filename,
+            "size" => $this->formatBytes($attachment->sizeInBytes),
+            "mime" => $attachment->mimeType
+        ];
+      }
+      $emailEntity->setAttachments($attachmentsArray);
+      $this->emailsRepository->save($emailEntity, true);
+    }
+    $connection->disconnect();
+
+    $allEmails = $this->emailsRepository->findAll();
+    return $this->json($allEmails);
+  }
+
+  private function formatBytes(?int $bytes): string {
+    $units = array('B', 'KB', 'MB', 'GB', 'TB');
+
+    $bytes = max($bytes, 0);
+    $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+    $pow = min($pow, count($units) - 1);
+
+    // Calculate the size and append the unit
+    $bytes /= pow(1024, $pow);
+
+    // Format the number with the desired precision
+    return round($bytes, 2) . ' ' . $units[$pow];
+  }
+}
